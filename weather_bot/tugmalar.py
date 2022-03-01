226150a97ce9d3a4cc1d10a9ec1a1006c3a2ae06
YUU <90501902+UmarProgrammer2021@users.noreply.github.com>
2022-03-01 23:11:24 +0500

    Add files via upload

---
+++ weather_bot/tugmalar.py
@@ -0,0 +1,12 @@
+from telegram import ReplyKeyboardMarkup
+
+hududlar_button=ReplyKeyboardMarkup([
+    ["Nukus","Urganch"],
+    ["Buxoro","Navoiy"],
+    ["Qarshi","Termiz"],
+    ["Samarqand","Jizzax"],
+    ["Guliston","Farg'ona"],
+    ["Andijon","Namangan"],
+    ["Toshkent Shaxri","Toshkent viloyati"]
+]
+)
